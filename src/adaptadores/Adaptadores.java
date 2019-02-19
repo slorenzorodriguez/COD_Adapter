bc5f8241793a6039b1b0cfb501592b01497d9cf3
Damian Nogueiras <damiannogueiras@gmail.com>
2019-02-19 07:01:08 +0100

    init

---
+++ src/adaptadores/Adaptadores.java
@@ -0,0 +1,28 @@
+package adaptadores;
+
+public class Adaptadores {
+
+    public static void main(String[] args) {
+        MediaPlayer player = new MP3();
+        player.play("file.mp3");
+        player = new FormatAdapter(new MP4());
+        player.play("file.mp4");
+        player = new FormatAdapter(new VLC());
+        player.play("file.avi");
+    }
+
+    private static class FormatAdapter implements MediaPlayer {
+
+        private MediaPackage media;
+
+        public FormatAdapter(MediaPackage m) {
+            media = m;
+        }
+
+        @Override
+        public void play(String filename) {
+            System.out.print("Using Adapter --> ");
+            media.playFile(filename);
+        }
+    }
+}
